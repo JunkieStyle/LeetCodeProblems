3b499ce968d60d94d25e4ffe473c88ba088a54d1
Anton Nekhaev <antonnekhaev@gmail.com>
2021-08-11 19:49:56 +0300

    problem 74

---
+++ bsearch/74_SearchA2DMatrix/main.py
@@ -0,0 +1,15 @@
+import bisect
+from typing import List
+
+
+class Solution:
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+        i = bisect.bisect([row[0] for row in matrix], target) - 1
+        if i < 0:
+            return False
+    
+        j = bisect.bisect(matrix[i], target) - 1
+        if j < 0:
+            return False
+        
+        return matrix[i][j] == target
