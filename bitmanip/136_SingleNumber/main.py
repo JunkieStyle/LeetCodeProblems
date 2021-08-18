fc20d179a263dcd309ba303bcec509eae36d2daf
Anton Nekhaev <antonnekhaev@gmail.com>
2021-08-18 05:58:24 +0300

    problem 136

---
+++ bitmanip/136_SingleNumber/main.py
@@ -0,0 +1,9 @@
+import operator
+
+from functools import reduce
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        return reduce(operator.xor, nums)
