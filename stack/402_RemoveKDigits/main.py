123b5063e035bb835da304c03c5755fca0d62ce2
Anton Nekhaev <antonnekhaev@gmail.com>
2021-09-01 11:48:11 +0300

    problem 402

---
+++ stack/402_RemoveKDigits/main.py
@@ -0,0 +1,16 @@
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        stack = []
+        
+        for i in range(len(num)):
+            while stack and stack[-1] > num[i] and k > 0:
+                stack.pop()
+                k -= 1
+            stack.append(num[i])
+            
+                    
+        while k:
+            stack.pop()
+            k -= 1
+
+        return "".join(stack).lstrip("0") or "0"
